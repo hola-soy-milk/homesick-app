227f3c294fad2c01cd137da6211d24a24385282a
ramon huidobro <ramon.martin.huidobro@gmail.com>
2014-10-28 17:37:23 +0100

    Implement audio recording

--- app/src/main/java/com/ramonh/homesick/RecordingActivity.java
+++ app/src/main/java/com/ramonh/homesick/RecordingActivity.java
@@ -1,17 +1,128 @@
 package com.ramonh.homesick;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.IOException;
 
 
 public class RecordingActivity extends Activity {
+
+    private static final String LOG_TAG = "RecordingActivity";
+    private static String mFileName = null;
+
+    private Button mRecordButton = null;
+    private MediaRecorder mRecorder = null;
+
+    private Button mPlayButton = null;
+    private MediaPlayer mPlayer = null;
+
+    private void onRecord(boolean start) {
+        if (start) {
+            startRecording();
+        } else {
+            stopRecording();
+        }
+    }
+
+    private void onPlay(boolean start) {
+        if (start) {
+            startPlaying();
+        } else {
+            stopPlaying();
+        }
+    }
+
+    private void startPlaying() {
+        mPlayer = new MediaPlayer();
+        try {
+            mPlayer.setDataSource(mFileName);
+            mPlayer.prepare();
+            mPlayer.start();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+    }
+
+    private void stopPlaying() {
+        mPlayer.release();
+        mPlayer = null;
+    }
+
+    private void startRecording() {
+        mRecorder = new MediaRecorder();
+        mRecorder.reset();
+        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mRecorder.setOutputFile(mFileName);
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            mRecorder.prepare();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+
+        mRecorder.start();
+    }
+
+    private void stopRecording() {
+        mRecorder.stop();
+        mRecorder.release();
+        mRecorder = null;
+    }
+
+    public RecordingActivity() {
+        mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
+        mFileName += "/audiorecordtest.3gp";
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recording);
+
+        mPlayButton = (Button)findViewById(R.id.play_button);
+        mRecordButton = (Button)findViewById(R.id.record_button);
+
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+
+            boolean mStartPlaying = true;
+
+            @Override
+            public void onClick(View view) {
+                onPlay(mStartPlaying);
+                if (mStartPlaying) {
+                    mPlayButton.setText("Stop playing");
+                } else {
+                    mPlayButton.setText("Start playing");
+                }
+                mStartPlaying = !mStartPlaying;
+            }
+        });
+
+        mRecordButton.setOnClickListener(new View.OnClickListener() {
+            boolean mStartRecording = true;
+            @Override
+            public void onClick(View view) {
+
+                onRecord(mStartRecording);
+                if (mStartRecording) {
+                    mRecordButton.setText("Stop recording");
+                } else {
+                    mRecordButton.setText("Start recording");
+                }
+                mStartRecording = !mStartRecording;
+            }
+        });
     }
 
 
@@ -32,5 +143,19 @@ public class RecordingActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mRecorder != null) {
+            mRecorder.release();
+            mRecorder = null;
+        }
+
+        if (mPlayer != null) {
+            mPlayer.release();
+            mPlayer = null;
+        }
     }
 }
